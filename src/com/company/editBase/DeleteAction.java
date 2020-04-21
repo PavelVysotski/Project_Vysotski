2e85c31a061305bd04db26c7733c64dadd73925c
pvysotski <pavelve83@mail.ru>
2020-04-21 13:59:42 +0300

    update

--- src/com/company/editBase/DeleteAction.java
+++ src/com/company/editBase/DeleteAction.java
@@ -2,6 +2,8 @@ package com.company.editBase;
 
 import com.company.CommandReader;
 import com.company.baseConnection.ComputersConnection;
+import com.company.baseConnection.CustomersConnection;
+import com.company.baseConnection.OrdersConnection;
 import com.company.showBase.ShowAllDb;
 
 import java.sql.SQLException;
@@ -16,12 +18,12 @@ public class DeleteAction {
     public static void deleteCustom() throws SQLException {
         ShowAllDb.showAllCustom();
         int customId = CommandReader.readNumber("Введите порядковый номер заказчика, который небходимо удалить: ");
-        ComputersConnection.deleteFromId(customId);
+      CustomersConnection.deleteFromId(customId);
     }
 
     public static void deleteOrders() throws SQLException {
         ShowAllDb.showAllOrders();
         int ordersId = CommandReader.readNumber("Введите порядковый номер заказа, который небходимо удалить: ");
-        ComputersConnection.deleteFromId(ordersId);
+        OrdersConnection.deleteFromId(ordersId);
     }
 }
