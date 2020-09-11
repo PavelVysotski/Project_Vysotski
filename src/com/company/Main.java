562cc4a8b209f83fb77517de88e05b8707481a3c
pvysotski <pavelve83@mail.ru>
2020-09-11 15:52:42 +0300

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,11 @@ package com.company;
 import com.company.menu.action.MainMenuChoice;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-
 
         MainMenuChoice.worker();
     }
